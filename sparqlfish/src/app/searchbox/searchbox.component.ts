7c62891a7b0d97e8ffd2500e71246d7599ea6367
Loïc Jaouen <loic.jaouen@unil.ch>
2018-10-27 16:23:19 +0200

    integrating npm pos package

--- sparqlfish/src/app/searchbox/searchbox.component.ts
+++ sparqlfish/src/app/searchbox/searchbox.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { pos } from 'pos';
+import { Lexer } from 'pos';
+import { Tagger } from 'pos';
 
 @Component({
   selector: 'app-searchbox',
@@ -9,10 +12,26 @@ import { FormsModule } from '@angular/forms';
 
 export class SearchboxComponent implements OnInit {
   searchtext: String;
+  value;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  evaluate(e: any) {
+    if (e.keyCode === 13) {
+      const words = new Lexer().lex(this.searchtext);
+      const tagger = new Tagger();
+      const taggedWords = tagger.tag(words);
+      this.value = taggedWords;
+
+      // for (const i of taggedWords) {
+      //     const taggedWord = taggedWords[i];
+      //     const word = taggedWord[0];
+      //     const tag = taggedWord[1];
+      //     this.value = this.value + '\
+      //     ' + word + ' /' + tag;
+    }
+  }
 }
