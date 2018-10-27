8d8647539fd54f9153ada0e11eb0d4e7ba7765c9
Loïc Jaouen <loic.jaouen@unil.ch>
2018-10-27 18:08:41 +0200

    parsing according to the tags

--- sparqlfish/src/app/searchbox/searchbox.component.ts
+++ sparqlfish/src/app/searchbox/searchbox.component.ts
@@ -12,7 +12,10 @@ import { Tagger } from 'pos';
 
 export class SearchboxComponent implements OnInit {
   searchtext: String;
-  value;
+  values: String[] = [];
+  entities: String[] = [];
+  concepts: String[] = [];
+  properties: String[] = [];
 
   constructor() { }
 
@@ -21,17 +24,32 @@ export class SearchboxComponent implements OnInit {
 
   evaluate(e: any) {
     if (e.keyCode === 13) {
+      this.values = [];
+      this.entities = [];
+      this.concepts = [];
+      this.properties = [];
       const words = new Lexer().lex(this.searchtext);
       const tagger = new Tagger();
       const taggedWords = tagger.tag(words);
-      this.value = taggedWords;
 
-      // for (const i of taggedWords) {
-      //     const taggedWord = taggedWords[i];
-      //     const word = taggedWord[0];
-      //     const tag = taggedWord[1];
-      //     this.value = this.value + '\
-      //     ' + word + ' /' + tag;
+      for (const taggedWord of taggedWords) {
+        const word = taggedWord[0];
+        const tag = taggedWord[1];
+        console.log('word: ' + word + ' : ' + tag);
+        if (tag.match(/^NNP[S]?/gi)) {
+          this.entities.push(word);
+          console.log('entities: ' + word);
+        } else if (tag.match(/^NN[S]?/gi)) {
+          this.concepts.push(word);
+          console.log('concepts: ' + word);
+        } else if (tag.match(/^VB/gi)) {
+          this.properties.push(word);
+          console.log('properties: ' + word);
+        }
+      }
+      this.values.push('potential entities: ' + this.entities);
+      this.values.push('potential concepts: ' + this.concepts);
+      this.values.push('potential properties: ' + this.properties);
     }
   }
 }
